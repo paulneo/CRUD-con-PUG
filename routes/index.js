c6a64be2372ce8c775e6a7c31291751d4045146d
paulneo <localos2000@gmail.com>
2017-05-21 18:26:40 -0500

    creando header.pug

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/header', function(req, res, next) {
+  res.render('header', { title: 'Express' });
+});
 
 module.exports = router;
